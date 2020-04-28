3ead5c6b624da6e94983e3bdf1f2f00a72de1e90
alberto-balsalm <wojciechpolitowski@protonmail.ch>
2020-04-28 14:48:45 +0200

    Basic sketch with no funcionality

---
+++ script.js
@@ -0,0 +1,25 @@
+
+function operate(a, b, operator) {
+    let result
+    if(operator == '+') result = add(a, b)
+    else if(operator == '-') result = subtract(a, b)
+    else if(operator == '*') result = multiply(a, b)
+    else if(operator == '/') result = divide(a,b)
+    return result
+}
+
+function add(a, b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b
+}
+
+function multiply (a, b) {
+    return a * b
+}
+
+function divide (a, b) {
+    return a / b
+}
