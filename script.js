14116d87c301f4f336b203f3fe6c8e51fc9c8efd
alberto-balsalm <wojciechpolitowski@protonmail.ch>
2020-04-30 17:16:21 +0200

    Performing caltulations logic

--- script.js
+++ script.js
@@ -7,18 +7,27 @@ function getInput() {
     let input = ""
     
     buttons.forEach(button => button.addEventListener('click', () => {
-        let chars = "0123456789"
         let operators = '−+×÷'
+        let numbers = ".0123456789"
         let currButton = button.textContent
         
+
         if(currButton == 'AC') {
+            input = ""
 
         } else if(currButton == '←') {
-            input = input.slice(0,input.length - 1)
+            //removing operator and spaces
+            if(input[input.length - 1] == " ")
+                input = input.slice(0,input.length - 3)
+            //removing a digit or a dot
+            else input = input.slice(0,input.length - 1)
         } else if(currButton == '=') {
-
-        } else if(button.classList[1] == 'operator') {
-            //previously input character was an operator -> replace it
+            //perform only if number is at the end of input
+            if(!operators.includes(input[input.length-2])) {
+                input = operate(input) //performing calculations
+            }
+        } else if(button.classList[1] == 'operator' && input.length) { 
+            //if previously input character was an operator -> replace it
             if(operators.includes(input[input.length-2])) {
                 input = input.substr(0, input.length-2) + currButton + " "                
             } 
@@ -26,7 +35,7 @@ function getInput() {
             else {
                 input += " " + currButton + " "
             }
-        } else {
+        } else if(numbers.includes(currButton)) { //just adding another digit 
             input += currButton
         }
          
@@ -36,21 +45,40 @@ function getInput() {
 
 }
 
+function operate(input) {
+    if(typeof(input) != "string") input = input.toString()
+    let inputArray = input.split(' ')
+    //performing multiplication and division first
+    for(let i = 0; i < inputArray.length; i++) {
+        switch(inputArray[i]) {
+            case '×':
+                inputArray.splice(i-1, 3, multiply(inputArray[i-1],inputArray[i+1]))
+                i = 0
+                break
+            case '÷':
+                inputArray.splice(i-1, 3, divide(inputArray[i-1], inputArray[i+1]))
+                i = 0
+                break
+        }
+    }
+    //addition and subtraction comes second
+    for(let i = 0; i < inputArray.length; i++) {
+        switch(inputArray[i]) {
+            case '+':
+                inputArray.splice(i-1, 3, add(parseInt(inputArray[i-1]),parseInt(inputArray[i+1])))
+                i = 0
+                break
+            case '−':
+                inputArray.splice(i-1, 3, add(parseInt(inputArray[i-1]),parseInt(inputArray[i+1])))
+                i = 0
+                break
+        }
+    }
+    
 
-
-
-function operate(a, b, operator) {
-    let result
-    if(operator == '+') result = add(a, b)
-    else if(operator == '-') result = subtract(a, b)
-    else if(operator == '*') result = multiply(a, b)
-    else if(operator == '/') result = divide(a,b)
-    return result
-
-
-
-
+    return inputArray[0]
 }
+
 
 function add(a, b) {
     return a + b
